7b7689930cf0b16654cd240f09faaea8882097b5
richmedina <rmedina@hawaii.edu>
2014-04-03 15:57:10 -1000

    reformatting

--- livethread/models.py
+++ livethread/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+
 class ThreadSpace(models.Model):
     name = models.CharField(max_length=128)
 
@@ -14,11 +15,8 @@ class ThreadSpace(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class ThreadMessage(models.Model):
     thread = models.ForeignKey(ThreadSpace)
     user = models.ForeignKey(User)
     text = models.TextField()
-
-
-
-
